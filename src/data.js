d0cf7efe79a1d16029501be62093114ea0cfa249
EKWA Rigobert <rigolaz@gmail.com>
2022-08-07 17:32:35 +0100

    dev electriciy

--- src/data.js
+++ src/data.js
@@ -1,95 +1,68 @@
 export const offre=[
     {
         id:1,
-        img: "https://res.cloudinary.com/do8lyndou/image/upload/v1652472241/electBeware_yzdqyq.svg",
         homoHeadColor:"#FFFFFF",
-        homoBodyColor:"#000000",
         title:"ELECTRICITE",
         desc:"Electricité domestique, publique, industrielle, biomédicale. Energie renouvelable.",
         lien: "/electricite",
-        bgi: "https://res.cloudinary.com/do8lyndou/image/upload/v1653849385/electricien_kymtcn.jpg"
+        bgi: "https://res.cloudinary.com/do8lyndou/image/upload/a_180/v1659815230/photo-1635335874521-7987db781153_nuqnn5.jpg"
     },{
         id:2,
-        img: "https://res.cloudinary.com/do8lyndou/image/upload/v1652845234/industrial-robot-automation-svgrepo-com_z5mrib.svg",
         homoHeadColor:"#FFFFFF",
-        homoBodyColor:"#000000",
         title:"INFORMATIQUE",
         desc:"Technique d'assistance, développement d'application web et mobile, informatique industrielle.",
         lien: "/informatique",
-        bgi: "https://res.cloudinary.com/do8lyndou/image/upload/v1654619661/informatique_zzclcl.jpg"
+        bgi: "https://res.cloudinary.com/do8lyndou/image/upload/v1659815108/photo-1561736778-92e52a7769ef_qzzfwd.jpg"
     },{
         id:3,
-        img: "https://res.cloudinary.com/do8lyndou/image/upload/v1652844924/gear_coice2.svg",
         homoHeadColor:"#FFFFFF",
-        homoBodyColor:"#000000",
         title:"ELECTRO MECANIQUE",
         desc:"Electroménager, ascenseur, mécanisme agricole, distribution automatique, etc...",
         lien: "/electromecanique",
-        bgi: "https://res.cloudinary.com/do8lyndou/image/upload/v1653849354/mecanique_omate2.jpg"
+        bgi: "https://res.cloudinary.com/do8lyndou/image/upload/v1659862487/%C3%A9lectrom%C3%A9canique_emdoxj.jpg"
     },{
         id:4,
-        img: "https://res.cloudinary.com/do8lyndou/image/upload/v1652845602/smart-home_ukasak.svg",
         homoHeadColor:"#FFFFFF",
-        homoBodyColor:"#000000",
         title:"DOMOTIQUE",
         desc:"Commande à distance de vos appareillages via le web (mobile, pc et autre...).",
         lien: "/domotique",
-        bgi: "https://res.cloudinary.com/do8lyndou/image/upload/v1653859075/domotique_riadpe.jpg"
+        bgi: "https://res.cloudinary.com/do8lyndou/image/upload/v1659879626/Domo-mini_xeqf4i.jpg"
     },{
         id:5,
-        img: "https://res.cloudinary.com/do8lyndou/image/upload/v1652845848/air-conditioner_sefatq.svg",
         homoHeadColor:"#FFFFFF",
-        homoBodyColor:"#000000",
         title:"FROID ET CLIMATISATION",
         desc:"Techniques d'assistance des climatiseurs, centrales thermiques, chambre froide, etc...",
         lien: "froid-et-climatisation",
-        bgi: "https://res.cloudinary.com/do8lyndou/image/upload/v1653849593/climatisation_q90enx.jpg"
+        bgi: "https://res.cloudinary.com/do8lyndou/image/upload/v1659816117/photo-1659164839265-9bb2f08aacb1_xjvjwv.jpg"
     },{
         id:6,
-        img: "https://res.cloudinary.com/do8lyndou/image/upload/v1652846126/plomberie_q9mhlu.svg",
         homoHeadColor:"#FFFFFF",
-        homoBodyColor:"#000000",
         title:"PLOMBERIE DOMESTIQUE ET INDUSTRIELLE",
         desc:"Tuyauterie domestique et industrielle, sanitaire, mécanique des fluides.",
         lien: "/plomberie",
-        bgi: "https://res.cloudinary.com/do8lyndou/image/upload/v1653850250/plomberie2_sltjc8.jpg"
+        bgi: "https://res.cloudinary.com/do8lyndou/image/upload/v1659817795/photo-1653379290878-1e839993509a_relqw5.jpg"
     },{
         id:7,
-        img: "https://res.cloudinary.com/do8lyndou/image/upload/v1652849645/video-surveillance_xc6rws.svg",
         homoHeadColor:"#FFFFFF",
-        homoBodyColor:"#000000",
         title:"VIDEO SURVEILLANCE",
         desc:"Surveillance adaptée avec possibilité de visualisation via vos appareils fixes et mobiles.",
         lien: "/video-surveillance",
-        bgi: "https://res.cloudinary.com/do8lyndou/image/upload/v1653850269/camera-surveillance_qnrrmg.jpg"
+        bgi: "https://res.cloudinary.com/do8lyndou/image/upload/v1659863928/vid%C3%A9osurveillance_m2flem.jpg"
     },{
         id:8,
-        img: "https://res.cloudinary.com/do8lyndou/image/upload/v1652847891/welding_z3dofc.svg",
         homoHeadColor:"#FFFFFF",
-        homoBodyColor:"#000000",
         title:"CHAUDRONERIE",
         desc:"Soudure aérienne, souterraine, sur plateforme et milieu aquatique.",
         lien: "/chaudronnerie",
-        bgi: "https://res.cloudinary.com/do8lyndou/image/upload/v1653849711/soudure_rlmter.jpg"
+        bgi: "https://res.cloudinary.com/do8lyndou/image/upload/v1659877478/Chaudronnier_edl6or.jpg"
     },{
         id:9,
-        img: "https://res.cloudinary.com/do8lyndou/image/upload/v1652850869/Altimeter_ota7bh.svg",
         homoHeadColor:"#FFFFFF",
-        homoBodyColor:"#000000",
         title:"AVIONIQUE",
         desc:"Techiques d'assistance dans l'aviation, gestion des systèmes embarqués.",
         lien: "/avionique",
         bgi: "https://res.cloudinary.com/do8lyndou/image/upload/v1654005226/avionique_iaxkrc.jpg"
-    }/* ,{
-        id:10,
-        img: "https://res.cloudinary.com/do8lyndou/image/upload/v1652848627/camion-grue_sdzknv.svg",
-        homoHeadColor:"#FFFFFF",
-        homoBodyColor:"#000000",
-        title:"BTP-URBANISME",
-        desc:"Bâtiments, travaux publics( éclairage public, les travaux souterrains, etc...).",
-        lien: "/btp",
-        bgi: "https://res.cloudinary.com/do8lyndou/image/upload/v1653850378/btp_sgkqdv.jpg"
-    } */
+    }
 ]
 
 export const backward =()=> {
--- src/pages/Electricite.jsx
+++ src/pages/Electricite.jsx
@@ -12,7 +12,7 @@ const Electricite = () => {
 	<Sidebar />
 <main>
 	<div id="marge"></div>
-	<div className='intro' style={{backgroundImage:`linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.7)), url("https://res.cloudinary.com/do8lyndou/image/upload/v1653849385/electricien_kymtcn.jpg")`}} >
+	<div className='intro' style={{backgroundImage:`linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.2)), url("https://res.cloudinary.com/do8lyndou/image/upload/a_180/v1659815230/photo-1635335874521-7987db781153_nuqnn5.jpg")`}} >
 		<div className='titre-home'>
 			<i onClick={backward} className="fa-solid fa-caret-left fa-xl"></i><h1>ELECTRICITE</h1>
 		</div>
@@ -23,7 +23,8 @@ const Electricite = () => {
             <div className="container">
 			{/* <!-- article body  --> */}
 			<div id="article-body" className="body">
-				<h2>ELECTRICITE DOMESTIQUE</h2>
+				<p>Saut direct aux <a href="#conseil"><strong>Conseils</strong></a> </p>
+				<img src="https://res.cloudinary.com/do8lyndou/image/upload/v1659882997/photo-1599709173687-64dcfed43386_wbtwll.jpg" alt="" /><h2>ELECTRICITE DOMESTIQUE</h2>
 				<p>C’est l’électricité du réseau électrique distribuée et utilisée dans les logements de particuliers ainsi que dans les lieux où les besoins sont similaires (Domicile, bureau, boutique, etc…). </p>
 				<h2>MAINTENANCE INDUSTRIELLE</h2>
 				<p>...Cette partie est en cours de développement pour des explications optimales par nos professionnels du métier...</p>
@@ -33,16 +34,23 @@ const Electricite = () => {
 				<p>...Cette partie est en cours de développement pour des explications optimales par nos professionnels du métier...</p>
 				<h2>Energie renouvelable</h2>
 				<p>...Cette partie est en cours de développement pour des explications optimales par nos professionnels du métier...</p>
+				<h2 id='conseil'> </h2>
+				<div id="marge"></div>
 				<h2 id='conseils'>Quelques conseils</h2>
 				<h3>Aménagement dans un nouveau local</h3>
-				<p>Lors de l’aménagement dans une nouvelle maison, Il faudrait tout d'abord demander une vérification minutieuse du compteur électrique présent sur le lieu par la société fournitrice de l'électricié, et dans le même ordre d'idée, demander un changement de nom pour que vous deveniez le nouveau propriétaire dudit compteur.
-					Pour besoin d'accompagenement, nous contacter.<br/> Ensuite, il est important, voire primordial de faire passer au peigne fin tout le circuit électrique par un professionnel du métier. Les circuits doivent non seulement respecter une certaine norme, mais aussi être en bon état car "prévenir vaut mieux que guerrir".</p>
+				<p>Lors de l’aménagement dans une nouvelle maison, il faudrait tout d'abord demander une vérification minutieuse du compteur électrique présent sur le lieu par la société fournitrice de l'électricié, et dans le même ordre d'idée, entamer la procédure de demande de changement de nom pour que vous deveniez le nouveau propriétaire dudit compteur. <br />
+					Pour tout besoin ponctuel d'accompagnement d'accompagenement, nous contacter directement <a href="tel:694088330">694 08 83 30</a>.<br/> Ensuite, il est important, voire primordial de faire passer au peigne fin tout le circuit électrique par un professionnel du métier. Les circuits doivent non seulement respecter une certaine norme, mais aussi être en bon état car "prévenir vaut mieux que guerrir".</p>
 				<h3>Les critères d'une installation aux normes</h3>
 				<h4>Sélectivité</h4>
-				<p>Une panne électrique survenue dans un local précis d'un bâtiment ne devrait pas affecter un autre local de près ou de loin. Il est donc besoin de faire *une sélectivité complète* afin d'éviter tout désagrément de ce type.</p>
+				<p>Une panne électrique survenue dans un local précis d'un bâtiment ne devrait pas en affecter un autre de près ou de loin pour quelque raison que ce soit. Il est donc besoin de faire *<strong>une sélectivité</strong>* afin d'éviter tout désagrément de ce type.</p>
 				<h4>Équilibrage de phases</h4>
-				<p>Un déséquilibre de votre réseau électrique interne pourrait engendrer la mauvaise qualité de l'électricité chez vous comme chez vos voisins, et donc techniquement générer un mauvais facteur de puissance.
+				<p>Un déséquilibre de votre réseau électrique interne pourrait engendrer la mauvaise qualité de l'électricité chez vous comme chez vos voisins, et donc peut techniquement générer un mauvais facteur de puissance.
 					Il faut noter que la société fournitrice de l'électricité pénalise tout ses clients ayant un mauvais facteur de puissance. La répercution est constatée dans les factures.</p>
+				<h4>Protection des personnes</h4>
+				<p>Toute construction devrait avoir des appareillages dédiés à la protection des personnes qui utilisent les différents appareils à carcasse métallique présents et accessibles tels que refrigérateurs, fer à repasser, cuisinière électrique, etc... d'où l'importance d'installer le dispositif de terre accompagné des disjoncteurs spécifiques.</p>
+				<h4>Circuits reprérés</h4>
+				<p>Un tableau électrique fait par un professionnel du métier devrait contenir des stickers permettant d'identifier l'appareillage de protection qui commande tel appareil ou tel local. Cette pratique permet d'anticiper dans les besoins futurs et aide quiconque, technicien comme simple usager, à vite se repérer lors d'une intervention quelconque. <br />
+				Tenez par exemple: Si un disjoncteur déclenche et qu'il est au préalable marqué dessus "Chauffe eau chambre parents", n'importe qui repérerait directement la source du problème et le dépannage serait donc plus aisé</p>
 			</div>
 			</div>
 				</div>
