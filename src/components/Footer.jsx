d2c2cec21f3112c85b6f141f47fcf248548297f2
EKWA Rigobert <rigolaz@gmail.com>
2022-08-21 15:46:54 +0100

    Removing BTP et urbanisme from Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,7 +18,7 @@ const Footer1 = () => {
         <li><NavLink to='/plomberie' className={({isActive}) =>(isActive ? 'nav-serv-active' : 'footer-list-anchor')} itemProp="significantLink">Plomberie</NavLink></li>
         <li><NavLink to='/courant-faible' className={({isActive}) =>(isActive ? 'nav-serv-active' : 'footer-list-anchor')} itemProp="significantLink">Courant faible</NavLink></li>
         <li><NavLink to='/chaudronnerie' className={({isActive}) =>(isActive ? 'nav-serv-active' : 'footer-list-anchor')} itemProp="significantLink">Chaudronnerie</NavLink></li>
-        <li><NavLink to='/btp' className={({isActive}) =>(isActive ? 'nav-serv-active' : 'footer-list-anchor')} itemProp="significantLink">BTP-Urbanisme</NavLink></li>
+        {/* <li><NavLink to='/btp' className={({isActive}) =>(isActive ? 'nav-serv-active' : 'footer-list-anchor')} itemProp="significantLink">BTP-Urbanisme</NavLink></li> */}
         <li><NavLink to='/avionique' className={({isActive}) =>(isActive ? 'nav-serv-active' : 'footer-list-anchor')} itemProp="significantLink">Avionique</NavLink></li>
       </ul>
       <ul className="footer-list-top">
