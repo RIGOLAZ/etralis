3fec23053f5fbe33f07c29aa9e2c82c1e2b919b7
rigolaz <rigolaz@gamil.com>
2023-01-24 09:17:05 +0100

    fixing Loca

--- src/pages/Localisation.jsx
+++ src/pages/Localisation.jsx
@@ -21,7 +21,7 @@ const Localisation = () => {
                   </div>
                 </div>
               </section>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d248.7493796289212!2d9.702845635853695!3d4.02242797780315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2scm!4v1673696289968!5m2!1sfr!2scm" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d248.7493796289212!2d9.702845635853695!3d4.02242797780315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2scm!4v1673696289968!5m2!1sfr!2scm" width="360" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </main>
             <Footer />
         </>
