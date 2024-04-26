a487249e6343b7faa1eeefa4caad8a1d682454b5
rigolaz <rigolaz@gamil.com>
2024-04-26 03:36:38 +0100

    Replacing phone icon target

--- src/components/ContactBanner.jsx
+++ src/components/ContactBanner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 const ContactBanner = () => {
     return (
         <>
-        <blockquote><p>Faites appel à <strong>ETRALIS</strong> au <a href="tel:677077030"><img id='tel' src="https://www.1ere-position.fr/wp-content/themes/m1press-theme/static/img/phone-new-blanc.svg" alt="phone" />+237 677077030</a> ou adressez-nous un courriel à <a href='mailto:support@etralis.com' itemProp="significantLink">support@etralis.com</a>.</p></blockquote>
+        <blockquote><p>Faites appel à <strong>ETRALIS</strong> au <a href="tel:677077030"><img id='tel' src="https://res.cloudinary.com/do8lyndou/image/upload/v1713588486/phone-solid_johtxs.svg" alt="phone" />  +237 677077030</a> ou adressez-nous un courriel à <a href='mailto:support@etralis.com' itemProp="significantLink">support@etralis.com</a>.</p></blockquote>
         </>
     );
 };
