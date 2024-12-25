81751f1cd0839121149bc1551b16be201b2f1c5e
rigolaz <68431157+RIGOLAZ@users.noreply.github.com>
2024-12-25 09:10:28 +0100

    xmas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,10 +9,11 @@ const Header = () => {
                 <Link to='/accueil'>
                     <img src="https://res.cloudinary.com/do8lyndou/image/upload/v1678701647/Button-ETRALIS_hcxo2a.svg" alt="strobe" />
                 <span style={{color:"white", fontWeight:"bold", paddingLeft:"10px", position:"absolute", top:"6px"}}>ETRALIS</span>
+            <img className='xmas' src="https://res.cloudinary.com/do8lyndou/image/upload/v1735091749/Chapeau-N%C3%B6el_vbieyp.png" alt="hat" />
                 </Link>
             </div>
             <button className="custom-btn btn-11">
-                <a href="http://shop.etralis.com/" target='_blank'>Go <span id='span1'>to eCommerce</span><span id='span2'>Shop</span><span id='span3'><img src="https://firebasestorage.googleapis.com/v0/b/etralis.appspot.com/o/tofs%2Fcart.svg?alt=media&token=ef30e85c-5d07-4ef3-b2d0-d688dcd43d27" alt="cart" /></span></a>
+                <a href="https://shop.etralis.com" target="_blank" rel='noreferrer'>Go <span id='span1'>to eCommerce</span><span id='span2'>Shop</span><span id='span3'><img src="https://firebasestorage.googleapis.com/v0/b/etralis.appspot.com/o/tofs%2Fcart.svg?alt=media&token=ef30e85c-5d07-4ef3-b2d0-d688dcd43d27" alt="cart" /></span></a>
             </button>
             <nav>
                 <NavLink to='/accueil' className={({isActive}) =>(isActive ? 'nav-active' : 'nav-inactive')}> Accueil </NavLink>
